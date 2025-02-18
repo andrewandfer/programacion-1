129186c897f6e6cac127e45c4aa5a6062c3d0bc1
Andres Ortiz <fernelya.ortizo@uqvirtual.edu.co>
2025-02-18 17:46:17 -0500

    isa fastidiosa

---
+++ src/Niño.java
@@ -0,0 +1,31 @@
+public class Niño {
+    private String nombre;
+    private String genero;
+    private int edad;
+    private String id;
+    private String alergias;
+    private String nombreAcudiente;
+    private String numeroAcudiente;
+
+    public Niño(String nombre,String genero,int edad,String id,String alergias,String nombreAcudiente,String numeroAcudiente){
+        this.nombre=nombre;
+        this.genero=genero;
+        this.edad=edad;
+        this.id=id;
+        this.alergias=alergias;
+        this.nombreAcudiente=nombreAcudiente;
+        this.numeroAcudiente=numeroAcudiente;
+
+    }
+    public String getNombre(){
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getGenero(){
+        return genero;
+        }
+    }
+
