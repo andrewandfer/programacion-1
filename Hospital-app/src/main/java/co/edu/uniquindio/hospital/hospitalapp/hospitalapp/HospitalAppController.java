924389dc4334e4a9d08a90996dfbfa35bab71644
andrewandfer <fernelyandres13@gmail.com>
2025-05-08 20:51:27 -0500

    v2

---
+++ Hospital-app/src/main/java/co/edu/uniquindio/hospital/hospitalapp/hospitalapp/HospitalAppController.java
@@ -0,0 +1,37 @@
+package co.edu.uniquindio.hospital.hospitalapp.hospitalapp;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class HospitalAppController {
+
+    @FXML
+    private Button btnAdministrador;
+
+    @FXML
+    private Button btnMedico;
+
+    @FXML
+    private Button btnPaciente;
+
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    void onAdministrador(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onMedico(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onPaciente(ActionEvent event) {
+
+    }
+
+}
